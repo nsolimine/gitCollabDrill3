a6a37af5fa6749e50c9eb537aab4d9f5fff8b5bf
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-04-11 10:21:00 -0600

    added addArray function to people.js file

--- people.js
+++ people.js
@@ -1,1 +1,11 @@
 var people = [{name: "chas", age: 17}, {name: "nick", age: 43}, {name: "malcolm", age: 87}]
+
+var numbers = [1, 2, 5, 7]
+
+function addArray(array) {
+  let total = 0;
+  for (let i=0; i<array.length; i++){
+    total = array[i] + total;
+  }
+  return total;
+}
